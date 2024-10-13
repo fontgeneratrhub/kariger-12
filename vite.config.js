b578c8ddab73d26cfcca2e5441171b4368a6dd52
fontgeneratrhub <fontgeneratorh@gmail.com>
2024-10-13 13:09:21 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // Define the root directory as public
   build: {
     rollupOptions: {
-      input: {
-        main: '/src/main.jsx', // Specify the input file explicitly
-      },
-    },
-  },
+      input: 'src/main.jsx' // Ensure this path is correct
+    }
+  }
 });
